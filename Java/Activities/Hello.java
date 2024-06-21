3797a48319551bb1476f7b3277e9130197491d32
AmolNikat33 <amol.nikat@ibm.com>
2024-06-21 07:31:00 +0530

    Add files via upload

---
+++ Java/Activities/Hello.java
@@ -0,0 +1,12 @@
+package com.example.fst_m1_java_03;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        System.out.println("Heelo Fst-m1");
+
+	}
+
+}
