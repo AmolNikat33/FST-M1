3f653a537af531fb399d15d70b4785f332a9f9ce
AmolNikat33 <amol.nikat@ibm.com>
2024-06-23 08:05:08 +0530

    Add files via upload

---
+++ Java/Activities/CustomException.java
@@ -0,0 +1,14 @@
+package com.example.collection;
+
+public class CustomException extends Exception {
+    private String message = null;
+ 
+    public CustomException(String message) {
+        this.message = message;
+    }
+ 
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
