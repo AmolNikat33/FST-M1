4cbb802600597676823fb2ed40962880501d6726
AmolNikat33 <amol.nikat@ibm.com>
2024-06-23 07:50:51 +0530

    Add files via upload

---
+++ Java/Activities/BicycleOperations.java
@@ -0,0 +1,7 @@
+package com.example.fst_m1_java_03;
+
+public interface BicycleOperations {
+	public void applyBrake(int decrement);
+    public void speedUp(int increment);
+}
+
---
+++ Java/Activities/BicycleParts.java
@@ -0,0 +1,7 @@
+package com.example.fst_m1_java_03;
+
+public interface  BicycleParts {
+	public int gears = 0;
+    public int currentSpeed = 0;
+}
+
